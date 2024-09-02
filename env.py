cd4315c9cf40a1af0a9ae9a28b7f8059c2e505de
Satyam Agrawal <65458419+satyam-1807@users.noreply.github.com>
2024-09-02 14:53:58 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,11 @@
 import os
+from qdrant_client import QdrantClient
+
+
 os.environ['OPENAI_API_KEY'] = 'your-openai-apikey'
+
+
+qdrant_client = QdrantClient(
+    url="add67a69-43d5-4bd9-b72b-194eba28b90a.europe-west3-0.gcp.cloud.qdrant.io:6333",
+    api_key=os.environ["AfCEWmPdM17JABsezQ45-ANIze_GvAf95vGv57dtpHaSpsZcDP-5ng"],
+)
